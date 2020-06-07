fbd8e2338f63b93c575361babd2c2212ebfb2264
AdbiyaSithwath <66269369+AdbiyaSithwath@users.noreply.github.com>
2020-06-07 15:10:21 +0530

    Minutes.java
    This file shows how to convert minutes into no.of years and days.
    
    Thank You!

--- Minutes.java
+++ Minutes.java
@@ -1,12 +1,13 @@
-import java.util.Scanner;
-public class MinDayYear{
+import java.util.*;
+public class MinYearDay{
    public static void main(String[] args){
-   Scanner input = new Scanner(System.in);
-   System.out.println("Enter no.of minutes:");
-   int minutes = input.nextInt();
-   int year = minutes/525600;
-   int remainingMinutes = minutes%525600;
-   int day = remainingMinutes/1440;
-   System.out.println("minutes+"Minutes is approximately"+year+"years and"+day+"days.");
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter no.of minutes:");//Enter no.of minutes:3456789
+      int minutes = sc.nextInt();
+      int year = minutes/525600;
+      int remMin = minutes%525600;
+      int day = remMin/1440;
+      System.out.println(minutes+"minutes is approximately"+year+"years and"+day+"days.");
+      //3456789 minutes is approximately 6years and 210days.
    }
 }
